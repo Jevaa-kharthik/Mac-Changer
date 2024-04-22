6dd91359049a90bc1dbe437e41b4ae5399328101
Jevaa Kharthik <jevaasureka@gmail.com>
2024-04-22 21:21:52 +0530

    New Version of mac changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,20 +1,39 @@
 import subprocess
+import re
 
-print("""
-  __  __                  ____ _                                 
- |  \/  | __ _  ___      / ___| |__   __ _ _ __   __ _  ___ _ __ 
- | |\/| |/ _` |/ __|____| |   | '_ \ / _` | '_ \ / _` |/ _ \ '__|
- | |  | | (_| | (_|_____| |___| | | | (_| | | | | (_| |  __/ |   
- |_|  |_|\__,_|\___|     \____|_| |_|\__,_|_| |_|\__, |\___|_|   
-                                                 |___/           
-""")
+def change_mac_address(interface, new_mac):
+    try:
+        # Check if the entered MAC address is valid
+        if not re.match(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$', new_mac):
+            print("Invalid MAC address format. Please enter a valid MAC address.")
+            return
 
-print("\n")
+        # Bring down the network interface
+        subprocess.call(["sudo", "ifconfig", interface, "down"])
 
-print("Author : Jevaa kharthik N")
-print("Contact : jevaakharthik@gmail.com")
-mac_address = input("Enter mac address to change : ")
+        # Change the MAC address
+        subprocess.call(["sudo", "ifconfig", interface, "ether", new_mac])
 
-subprocess.call("sudo ifconfig en0 down", shell=True)
-subprocess.call("sudo ifconfig en0 hw ether {}".format(mac_address), shell=True)
-subprocess.call("sudo ifconfig en0 up", shell=True)
+        # Bring the interface back up
+        subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+        print(f"MAC address of {interface} changed successfully to {new_mac}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    print("""
+      __  __                  ____ _
+     |  \/  | __ _  ___      / ___| |__   __ _ _ __   __ _  ___ _ __
+     | |\/| |/ _` |/ __|____| |   | '_ \ / _` | '_ \ / _` |/ _ \ '__|
+     | |  | | (_| | (_|_____| |___| | | | (_| | | | | (_| |  __/ |
+     |_|  |_|\__,_|\___|     \____|_| |_|\__,_|_| |_|\__, |\___|_|
+                                                     |___/
+    """)
+    print("Author : Jevaa kharthik N")
+    print("Contact : jevaakharthik@gmail.com")
+    interface = input("Enter the network interface name (e.g., en0): ")
+    new_mac = input("Enter the new MAC address: ")
+    change_mac_address(interface, new_mac)
+
